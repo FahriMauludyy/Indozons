f9fb56f4614bb3ae0a2b74ec3965312097940edf
FahriMauludyy <idnmuhammadfahri2008@gmail.com>
2024-09-26 14:10:39 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    theme: {
+      extend: {
+        translate: {
+          'full': '100%',
+        },
+      },
+    },
+    variants: {
+      extend: {
+        translate: ['responsive', 'hover', 'focus', 'group-hover'],
+      },
+    },
+    plugins: [],
+  }
+  
